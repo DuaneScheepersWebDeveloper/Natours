8ffa94b117ab4964bea8ff0056dfd2fffaeb2015
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2024-05-18 18:31:21 +0200

    update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const app = express();
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 app.use(express.json());
 
 //1.Middleware
