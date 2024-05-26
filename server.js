ce9a8fa065f2d3e8f9fc9460f16343db36a84e45
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2024-05-26 20:49:26 +0200

    update

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+const dotenv = require('dotenv');
 
-const port = 3000;
+dotenv.config({ path: './config.env' });
+console.log(process.env.DATABASE); // Check if this logs the correct value
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+//const localDatadbase = process.env.DATABASE_LOCAL;
+//const onlineDatadbase = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then((con) => {
+    console.log(con.connection);
+    console.log('DB connection successful');
+  });
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
