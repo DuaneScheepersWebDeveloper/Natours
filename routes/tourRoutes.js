cbfe7e2510357d0bc7f7fd39ea7bcea1fd90d87c
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2024-06-01 19:35:59 +0200

    update

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
-
 const router = express.Router();
-
-router.param('id', tourController.checkID);
-
-router.param('name', tourController.checkBody);
-
-router.route('/').post(tourController.checkBody, tourController.addTour);
+//router.param('id', tourController.checkID);
+//router.param('name', tourController.checkBody);
+router
+  .route('/')
+  .post(tourController.createTour)
+  .get(tourController.getAllTours);
 
 router
   .route('/:id')
-  .get(tourController.getTour)
   .patch(tourController.updateTour)
   .delete(tourController.deleteTour);
 
--- server.js
+++ server.js
@@ -3,25 +3,26 @@ const app = require('./app');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-console.log(process.env.DATABASE); // Check if this logs the correct value
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
-
 //const localDatadbase = process.env.DATABASE_LOCAL;
 //const onlineDatadbase = process.env.DATABASE;
-
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true, // Add this line
+    ssl: true, // Add this line if you're using SSL
   })
-  .then((con) => {
-    console.log(con.connection);
+  .then(() => {
     console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err);
   });
 
 const port = process.env.PORT || 3000;
