7a94d51c9e457c74802ee75ff79d298f7ded10df
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2024-06-07 05:58:30 +0200

    Update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,28 +2,31 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    //Build the Query
-    //1)Filtering
+    // Build the Query
+    // 1 a) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    //2) Advanced Filtering
-    const queryString = JSON.stringify(queryObj);
-    queryString.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryString));
-    const query = await Tour.find(queryObj);
-    console.log(req.query);
-    //{difficulty:'easy',duration:{%gte:5}}
+    // 1 b) Advanced Filtering
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`,
+    );
+    const queryObjParsed = JSON.parse(queryString);
 
-    // const query = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-    //Execute the query
+    // 2) Sorting
+    let query = Tour.find(queryObjParsed);
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    }
+
+    // Execute the query
     const tours = await query;
-    //Send Response
+
+    // Send Response
     res.status(200).json({
       status: 'success',
       results: tours.length,
