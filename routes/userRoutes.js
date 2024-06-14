0bb03bb003cf5999bb7e47eec98f1d08b0399644
Duane Scheepers <duanes@eclipsetechnologies.co.za>
2024-06-14 16:01:11 +0200

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+router.post('/signup',authController.signup)
 
-router.route('/').get(userController.getAllUsers).post(userController.addUser);
+router.route('/')
+.get(userController.getAllUsers)
+.post(userController.addUser);
 router
   .route('/:id')
   .get(userController.getUser)
