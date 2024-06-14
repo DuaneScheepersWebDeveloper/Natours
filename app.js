3196431c74eed745c96e64cffdf38b8e2a06fdb4
Duane Scheepers <duanes@eclipsetechnologies.co.za>
2024-06-14 14:05:50 +0200

    updated the error handling

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-app.use(express.json());
+const app = express();
 
-//1.Middleware
-app.use(morgan('dev'));
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next(); //always call next
-});
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*',(req,res,next)=>{
-next(new AppError(`Can not find ${req.originalUrl} on this server!`));
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-  app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 module.exports = app;
-//npm install -g ndb
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -25,6 +31,14 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
