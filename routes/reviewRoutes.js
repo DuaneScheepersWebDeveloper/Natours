068a0609bf8e1816d8e09434e4d3a9a3573a1466
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2024-07-15 06:17:30 +0200

    update

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,14 +2,26 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router(); // Corrected from express.router to express.Router()
+const router = express.Router({ mergeParams: true });
+
+router.route('/').get(reviewController.getAllReviews).post(
+  authController.protect,
+  authController.restrictTo('user'), // Make sure this role matches the user's role
+  reviewController.createReview,
+);
+
 router
-  .route('/')
-  .get(reviewController.getAllReviews)
-  .post(
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(
     authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview,
+    authController.restrictTo('user', 'admin'), // Ensure roles match for update
+    reviewController.updateReview,
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('user', 'admin'), // Ensure roles match for delete
+    reviewController.deleteReview,
   );
 
 module.exports = router;
