2898b9287a47be5d1daf5e30f932713ff4d8c0a1
Duane Scheepers <duanes@eclipsetechnologies.co.za>
2024-09-27 13:52:55 +0200

    fixed the signup problem

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,42 +3,25 @@ const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
 const router = express.Router();
-router.use(authController.protect);
-router.use(authController.restrictTo('admin'));
-router.get(
-  '/me',
 
-  userController.getMe,
-  userController.getUser,
-);
+// Public routes (no protection)
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
+// Protected routes (authentication required)
+router.use(authController.protect);
 
-  authController.updatePassword,
-);
-
+router.patch('/updateMyPassword', authController.updatePassword);
 router.patch('/updateMe', userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
-router
-  .route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+// Routes restricted to admin only
+router.use(authController.restrictTo('admin'));
 
-router
-  .route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+router.get('/me', userController.getMe, userController.getUser);
+router.route('/').get(userController.getAllUsers).post(userController.createUser);
+router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
 module.exports = router;
-
-//POST / tour/234fhad/reviews
-//GET / tour/234fhad/reviews
-//GET / tour/234fhad/reviews/ahwbhbqwad
