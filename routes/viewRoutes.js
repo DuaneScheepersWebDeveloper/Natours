b0d632056cf72e1a2539d6d0a89a92cf2adb54c8
Duane Scheepers <duanes@eclipsetechnologies.co.za>
2025-01-06 08:38:56 +0200

    update

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,10 @@
 const viewController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 const express = require('express');
 
 const router = express.Router();
 router.get('/',viewController.getOverview);
-router.get('/tour/:slug',viewController.getTour);
+router.get('/tour/:slug',authController.protect,viewController.getTour);
 router.get('/login',viewController.getLoginForm);
 
 module.exports = router;
